4d1640fa96161047d99b69445b569c7f908a1011
Depiction <nicalcoca@gmail.com>
2024-03-24 16:52:35 -0500

    CSS for basic animations, no mames

--- static/js/home.js
+++ static/js/home.js
@@ -1,7 +1,1 @@
-const imageContainer = document.querySelector('.image-container');
-const imageWrapper = imageContainer.querySelectorAll('.image-wrapper');
-
-imageWrapper.forEach(img => {
-    const clone = img.cloneNode(true); // Clone the image
-    imageContainer.appendChild(clone); // Append the clone after the original image
-});
+console.log("XD")
