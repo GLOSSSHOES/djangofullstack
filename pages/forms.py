fb4b61653a3c6780d89e83096066f1e5d78492df
DEPICTION <nicalcoca@gmail.com>
2024-03-26 11:49:40 -0500

    Messages for stakeholders done

--- pages/forms.py
+++ pages/forms.py
@@ -1,6 +1,17 @@
 from django import forms
 
-class ContactForm(forms.Form):
+from .models import StakeholderMessage
+
+
+class StakeFolderMessageForm(forms.ModelForm):
+    class Meta:
+        model = StakeholderMessage
+        exclude = [
+            "id",
+            "related_user",
+            "user_is_anonymous",
+        ]
+
     name = forms.CharField(label="Tu nombre", max_length=100)
     email = forms.EmailField(label="Tu correo electrónico")
     message = forms.CharField(label="Tu inquietud", widget=forms.Textarea)
--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import HomePageView, ContactFormView, BrandView
+from .views import HomePageView, BrandView, CreateStakeHolderMessage
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
-    path("contacto/", ContactFormView.as_view(), name="contact"),
+    path("contacto/", CreateStakeHolderMessage.as_view(), name="contact"),
     path("marca/", BrandView.as_view(), name="brand"),
 ]
